dd958bd74bb76242cd92eef31e4c49b09f875d2d
Hugo Bove <hugo.bove@neotechnology.com>
2019-08-23 13:32:52 +0200

    moved files

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export {default as parseLambda} from './parse-lambda'
--- src/parse-lambda.js
+++ src/parse-lambda.js
@@ -3,7 +3,7 @@ import nearley from 'nearley';
 import grammar from './grammar';
 
 export default function parseLambda(string) {
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar), {keepHistory: true});
 
     parser.feed(string);
 
