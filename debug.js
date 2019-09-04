e43f9a890f28cfd980d9b077e755797f1326bba4
Hugo Bove <hugo.bove@neotechnology.com>
2019-09-04 14:06:22 +0200

    Killing a kitten - Removed all parsing of return statements - All tests are failing - Ambiguity increased to 2 - Hopefully we can come back and try to address this once we have Cypher parser available

--- debug.js
+++ debug.js
@@ -1,4 +1,4 @@
-import parseLambda from './parse-lambda';
+import {parseLambda} from './src';
 
 const something = `[{x}] => { CALL db.labels() YIELD label
 
@@ -9,10 +9,10 @@ foo
 
 RETURN 1
 }`;
-const result1 = parseLambda(something)
-const result2 = parseLambda('x => {asdsd RETURN rand() }')
-const result3 = parseLambda('[{rand():x}] => { RETURN rand() AS bar }')
-const result4 = parseLambda('x1 => "foo"')
+// const result1 = parseLambda(something)
+const result2 = parseLambda('x => { \n \n \n asdsd RETURN rand() }')
+// const result3 = parseLambda('[{rand():x}] => { RETURN rand() AS bar }')
+// const result4 = parseLambda('x1 => "foo"')
 
-console.log(JSON.stringify(result1, null, 2));
-console.log(`Ambiguity ${result1.length}`)
+console.log(JSON.stringify(result2, null, 2));
+console.log(`Ambiguity ${result2.length}`)
